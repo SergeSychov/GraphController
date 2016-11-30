fa205d574480290b06816b7b80bd4bd0e4403134
SSF <SSF@MacBook-Pro-SSF.local>
2016-11-30 11:05:01 +0300

    Initial Commit

---
+++ TestNSConstrain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestNSConstrain
+//
+//  Created by SSF on 30.11.16.
+//  Copyright © 2016 SSF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestNSConstrain/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestNSConstrain
+//
+//  Created by SSF on 30.11.16.
+//  Copyright © 2016 SSF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
